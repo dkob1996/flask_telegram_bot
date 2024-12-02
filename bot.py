614844b8b40624a7e1642fe74beb3e4798f31fa0
Dmitrii Kobozev <dmitrii_kobozev@Noutbuk-Dmitrii.local>
2024-12-02 11:45:45 +0600

    correct bot

--- bot.py
+++ bot.py
@@ -2,3 +2,12 @@ import telebot
 
 TOKEN = "7844253183:AAEYCdClT8GPhYm6q3-pjKwB_rBIVM8zjG8"
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+bot.send_message(chat_id="@ВАШ_ЮЗЕРНЕЙМ_КАНАЛА", text="Привет из бота!")
+
+bot.polling(none_stop=True)
+
+
+# python3 -m venv myenv
+# source myenv/bin/activate  # Активировать виртуальное окружение
+# pip install pyTelegramBotAPI
