596c61cb3e83f36503ec2ef3fada1b4c5e1da83c
Dkob <dkob1996@gmail.com>
2025-02-22 14:04:46 +0600

    trying to fix

--- bot.py
+++ bot.py
@@ -14,8 +14,8 @@ CHAT_ID = config["chat_id"]
 SERVER_URL = config["server_url"]
 PORT = config["server_port"]
 '''
-TOKEN = os.environ.get("BOT_TOKEN")
-CHAT_ID = os.environ.get("CHAT_ID")
+TOKEN = str(os.environ.get("BOT_TOKEN"))
+CHAT_ID = str(os.environ.get("CHAT_ID"))
 SERVER_URL = os.environ.get("RAILWAY_PUBLIC_DOMAIN")
 PORT = int(os.environ.get("SERVER_PORT", 5000))
 
