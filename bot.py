50f79219a33ea18ee8b96617475595d6a677f323
Dkob <dkob1996@gmail.com>
2025-02-22 14:08:21 +0600

    trying to fix

--- bot.py
+++ bot.py
@@ -14,15 +14,13 @@ CHAT_ID = config["chat_id"]
 SERVER_URL = config["server_url"]
 PORT = config["server_port"]
 '''
-TOKEN = str(os.environ.get("BOT_TOKEN"))
-CHAT_ID = str(os.environ.get("CHAT_ID"))
+TOKEN = os.environ.get("BOT_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
 SERVER_URL = os.environ.get("RAILWAY_PUBLIC_DOMAIN")
 PORT = int(os.environ.get("SERVER_PORT", 5000))
 
-print(TOKEN)
-print(SERVER_URL)
-print(PORT)
-print(CHAT_ID)
+if not TOKEN:
+    raise ValueError("BOT_TOKEN environment variable is not set!")
 
 bot = Bot(token=TOKEN)
 app = Flask(__name__)
